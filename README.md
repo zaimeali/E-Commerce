00cbc4a99b2e2a5534092133fb48c3558b97c7d6
zaimeali <zaimeali1997@gmail.com>
2021-02-14 19:05:50 -0500

    Nodemon install

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 In Backend:
 1) npm init
 2) npm i express
+3) node index.js
+4) npm i nodemon
+5) change in script in package.json
+6) "start" => "nodemon index.js"
+7) now npm start in terminal
--- testback/index.js
+++ testback/index.js
@@ -4,6 +4,9 @@ const app = express()
 
 const port = 3000
 
-app.get('/', (res, req) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/login', (req, res) => res.send('You are Logged In'))
+app.get('/signout', (req, res) => res.send('You are Logged Out'))
+app.get('/name', (req, res) => res.send('Your name is Zaime'))
 
 app.listen(port, () => console.log(`Listening on Port ${ port }`))
