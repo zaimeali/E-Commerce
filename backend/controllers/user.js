a9b8ef6e10dbf9830fe56e96cd1195127091789f
zaimeali <zaimeali1997@gmail.com>
2021-02-19 14:37:33 -0500

    populate function of mongodb

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('./../models/user')
+const Order = require('./../models/order')
 
 exports.getUserById = (req, res, next, id) => {
     User.findById(id)
@@ -53,6 +54,51 @@ exports.updateUser = (req, res) => {
                     email: user.email,
                 }
             })
+        }
+    )
+}
+
+exports.userPurchaseList = (req, res) => {
+    Order.find({ user: req.profile._id })
+        .populate("user", "_id name")
+        .exec((err, order) => {
+            if(err) {
+                return res.status(400)
+                    .json({
+                        error: "No order in this account"
+                    })
+            }
+            return res.json(order)
+        })
+}
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+    let purchases = []
+    req.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id
+        })
+    })
+
+    // Store in dB
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $push: { purchases: purchases } },
+        { new: true },  // it will send the updated one
+        (err, purchases) => {
+            if(err) {
+                return res.status(400)
+                    .json({
+                        error: "Unable to save purchase list"
+                    })
+            }
+            next()
         }
     )
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Controller
-const { getUserById, getUser, getAllUser, updateUser } = require('./../controllers/user')
+const { getUserById, getUser, getAllUser, updateUser, userPurchaseList } = require('./../controllers/user')
 const { isSignedIn, isAuthenticated, isAdmin } = require("./../controllers/auth")
 
 router.param("userId", getUserById)
@@ -10,6 +10,9 @@ router.param("userId", getUserById)
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser)
 
 router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser)
+
+
+router.get("/orders/user/:userId", isSignedIn, isAuthenticated, userPurchaseList)
 
 // router.get("/alluser", getAllUser)
 
