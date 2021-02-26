39dc2f3715822b1334f81a23fa3755bbc8390c4f
zaimeali <zaimeali1997@gmail.com>
2021-02-26 10:08:42 -0500

    Create Product API Done

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,34 +1,77 @@
-const Product = require('./../models/product')
+const Product = require("./../models/product");
 
 // formidable
-const formidabale = require('formidable')
+const formidabale = require("formidable");
 
 // lodash
-const _ = require('lodash')
+const _ = require("lodash");
 
 // FileSystem
-const fs = require('fs')
-
+const fs = require("fs");
 
 exports.getProductById = (req, res, next, id) => {
-    Product.findById(id)
-        .populate("category")
-        .exec((err, product) => {
-            if(err) {
-                return res.status(400)
-                    .json({
-                        error: "No Product Found"
-                    })
-            }
-            req.product = product
-            next()
-        })
-}
+  Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
+      if (err) {
+        return res.status(400).json({
+          error: "No Product Found",
+        });
+      }
+      req.product = product;
+      next();
+    });
+};
 
 exports.getProduct = (req, res) => {
-    return res.json(req.product)
-}
+  return res.json(req.product);
+};
 
 exports.createProduct = (req, res) => {
-    let form = new formidabale.IncomingForm()
-}
+  let form = new formidabale.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Problem with Image",
+        message: err,
+      });
+    }
+
+    //   Destructure Fields
+    const { name, description, price, category, stock } = fields;
+
+    if (!name || !description || !price || !category || !stock) {
+      return res.status(400).json({
+        error: "Please include all fields",
+      });
+    }
+
+    let product = new Product(fields);
+
+    // handle file here
+    if (file.photo) {
+      if (file.photo.size > 3000000) {
+        return res.status(400).json({
+          error: "File size is too big!",
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    // Save to the dB
+    product.save((err, product) => {
+      if (err) {
+        return res.status(400).json({
+          error: "Something got wrong in saving product",
+          message: err,
+        });
+      }
+      res.json({
+        product,
+      });
+    });
+  });
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // Controller
-const { getProductById, getProduct, createProduct } = require('../controllers/product')
-const { isSignedIn, isAuthenticated, isAdmin } = require('./../controllers/auth')
-const { getUserById } = require('./../controllers/user')
+const {
+  getProductById,
+  getProduct,
+  createProduct,
+} = require("../controllers/product");
+const {
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+} = require("./../controllers/auth");
+const { getUserById } = require("./../controllers/user");
 
 // Params
-router.param('productId', getProductById)
-router.param('userId', getUserById)
+router.param("productId", getProductById);
+router.param("userId", getUserById);
 
 // Routes
-router.get('/product/:productId', getProduct)
+router.get("/product/:productId", getProduct);
 
-router.post('/product/create/:userId', isSignedIn, isAuthenticated, isAdmin, createProduct)
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
 
-module.exports = router
+module.exports = router;
