35e4cac09bab6e9a04d8d76e9513b74f14d687b4
zaimeali <zaimeali1997@gmail.com>
2021-03-06 00:36:13 -0500

    Section 11 Done && Should revise tomorrow

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,48 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
+const express = require("express");
 
 // dB
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Middleware
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // Routes
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
-const categoryRoutes = require('./routes/category')
-const productRoutes = require('./routes/product')
-
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
 
 // dB Connection
-mongoose.connect(process.env.DATABASE,{
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-}).then(() => {
-    console.log("DB connected")
-}).catch(err => console.error(err))
+  })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => console.error(err));
 
-const app = express()
+const app = express();
 
 // Middleware in Express
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 // Auth Router
-app.use("/api", authRoutes)
-app.use("/api", userRoutes)
-app.use("/api", categoryRoutes)
-app.use("/api", productRoutes)
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log("Real Backend Server is up and running"))
+app.listen(port, () => console.log("Real Backend Server is up and running"));
--- backend/models/order.js
+++ backend/models/order.js
@@ -1,39 +1,43 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const { ObjectId } = mongoose.Schema
+const { ObjectId } = mongoose.Schema;
 
 const productCartSchema = new Schema({
-    product: {
-        type: ObjectId,
-        ref: "Product"
-    },
-    name: String,
-    count: Number,
-    price: Number
-})
+  product: {
+    type: ObjectId,
+    ref: "Product",
+  },
+  name: String,
+  count: Number,
+  price: Number,
+});
 
-const orderSchema = new Schema({
-    products: [
-        productCartSchema
-    ],
-    transaction_id: {
-
-    },
+const orderSchema = new Schema(
+  {
+    products: [productCartSchema],
+    transaction_id: {},
     amount: {
-        type: Number
+      type: Number,
     },
     address: String,
+    status: {
+      type: String,
+      default: "Received",
+      enum: ["Cancelled", "Delivered", "Shipped", "Processing", "Received"],
+    },
     updated: Date,
     user: {
-        type: ObjectId,
-        ref: "User"
-    }
-}, {
-    timestamps: true
-})
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Order = mongoose.model("Order", orderSchema)
-const ProductCart = mongoose.model("ProductCart", productCartSchema)
+const Order = mongoose.model("Order", orderSchema);
+const ProductCart = mongoose.model("ProductCart", productCartSchema);
 
-module.exports = { Order, ProductCart }
+module.exports = { Order, ProductCart };
