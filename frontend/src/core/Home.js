e6b6ac445a29ff1855177b762b358c25e70f12b2
zaimeali <zaimeali1997@gmail.com>
2021-03-07 22:40:27 -0500

    Section 14 Start

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -4,9 +4,26 @@ import React from "react";
 import { API } from "../backend";
 
 // Styles
-// import "./../styles.css";
+import "./../styles.css";
+
+// Components
+import Base from "./Base";
 
 export default function Home() {
   console.log("API is: ", API);
-  return <div>Home {API}</div>;
+  return (
+    <Base title="Home Page">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">Test</button>
+        </div>
+      </div>
+    </Base>
+  );
 }
